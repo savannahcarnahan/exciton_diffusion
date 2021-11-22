222713e8ea1e622b923e55bbfcc706ede7aa0a65
savannahcarnahan <sdc2@princeton.edu>
2021-11-22 11:03:45 -0500

    added marcus.py

--- marcus.py
+++ marcus.py
@@ -1,16 +1,18 @@
 import prob_rule as p
 import system
-class Arrhenius:
+import math
+class Marcus:
     
     # creates the correct probability rule
     def __init__(self):
         pass
 
     # calculates the probability with the equation
-    # k_12 = v_eff * e^((-1/kb*T)*delta_G - correction_factor)
-    # see https://pubs.acs.org/doi/pdf/10.1021/acs.chemrev.7b00086 p. 6
+    # k_12 = 2*pi/hbar(1/sqrt(4*pi*lambda*kb*T)*H_12^2*e^(-1/kb*T*delta_G))
+    # tunneling is ignored
+    # see https://pubs.acs.org/doi/pdf/10.1021/acs.chemrev.7b00086 p. 6-7
     def transition_prob(site1, site2, system):
         p.transition_prob(site1, site2)
         # place holder until implementation
-        prob = 1
+        prob = 0.0001
         return prob

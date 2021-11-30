3fd6213048c628941b23f217aca0d2971526b6b2
savannahcarnahan <sdc2@princeton.edu>
2021-11-30 14:42:36 -0500

    someday someone will know I modified pointparticle

--- pointparticle.py
+++ pointparticle.py
@@ -2,8 +2,12 @@ import atom
 
 class PointParticle(Atom):
 
-    def __init__(self):
+    def __init__(self, coord):
         self.is_excited_ = False
+        self.position = coord
+
+    def position():
+        return position
     
     def transition_probability(system):
         pass

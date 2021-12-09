348e1de91eb749ae884f584b172b387d69a3c4f8
savannahcarnahan <sdc2@princeton.edu>
2021-12-09 18:02:38 -0500

    wrote test for crystal module

---
+++ tests/test_crystal.py
@@ -0,0 +1,16 @@
+import numpy as np
+import crystal
+import system
+import mysite as site
+import pointparticle as pt
+
+def test_crystal():
+
+    site1 = pt.PointParticle(np.array([1,0,0]))
+
+    site2 = pt.PointParticle(np.array([0,1,0]))
+
+    syst = crystal.Crystal([site1, site2], 3, 'Arrhenius')
+    assert issubclass(crystal.Crystal, system.System)
+
+    setattr(site1,'excited',True)

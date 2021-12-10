7a69a68b8fae7efa9f7628868f8c0eb5c72b1e11
savannahcarnahan <sdc2@princeton.edu>
2021-12-10 11:59:02 -0500

    added test for inputprocessor

---
+++ tests/test_inputprocessor.py
@@ -0,0 +1,11 @@
+from inputprocessor import process_input
+from pointparticle import PointParticle
+def test_inputprocessor(in_file = 'test_input.txt'):
+    system_type, site_list, dimen, rate, model_type, start_time, end_time = process_input(in_file)
+    assert system_type == 'crystal'
+    assert dimen == 3
+    assert rate == 'marcus'
+    assert model_type == 'kmc'
+    assert start_time == 0
+    assert end_time == 10
+    assert isinstance(site_list[0], PointParticle)

75de9af4bcbd5a5252879174a1fbf760c8607eae
savannahcarnahan <sdc2@princeton.edu>
2021-12-11 18:39:39 -0500

    fixed variable name

--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -10,4 +10,4 @@ def test_pointparticle():
     atom_list.append(ase.Atom('N',np.array([1.08,0,0])))
     molec = m.Molecule(*atom_list)
     assert np.array_equal(molec.get_position(), np.array([0.54,0,0])) 
-    assert isinstance(p, site.Site)
+    assert isinstance(molec, site.Site)

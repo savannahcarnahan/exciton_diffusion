44a99f22a62f869e4fac5bf93492080ae9b91d1a
savannahcarnahan <sdc2@princeton.edu>
2021-12-12 12:30:26 -0500

    added tests for molecule

--- tests/test_inputprocessor.py
+++ tests/test_inputprocessor.py
@@ -1,6 +1,8 @@
 from inputprocessor import process_input
 from pointparticle import PointParticle
-def test_inputprocessor(in_file = 'test_input.txt'):
+from molecule import Molecule
+def test_inputprocessor(in_file = 'test_input.txt', in_file2 = 'test_input2.txt',
+        in_file3 = 'test_molecule.txt'):
     system_type, site_list, dimen, rate, model_type, start_time, end_time = process_input(in_file)
     assert system_type == 'crystal'
     assert dimen == 3
@@ -9,3 +11,22 @@ def test_inputprocessor(in_file = 'test_input.txt'):
     assert start_time == 0
     assert end_time == 10
     assert isinstance(site_list[0], PointParticle)
+
+    system_type, site_list, dimen, rate, model_type, start_time, end_time = process_input(in_file2)
+    assert system_type == 'crystal'
+    assert dimen == 3
+    assert rate == 'arrhenius'
+    assert model_type == 'kmc'
+    assert start_time == 0
+    assert end_time == 10
+    assert isinstance(site_list[0], PointParticle)
+
+
+    system_type, site_list, dimen, rate, model_type, start_time, end_time = process_input(in_file3)
+    assert system_type == 'crystal'
+    assert dimen == 3
+    assert rate == 'marcus'
+    assert model_type == 'kmc'
+    assert start_time == 0
+    assert end_time == 10
+    assert isinstance(site_list[1], Molecule)

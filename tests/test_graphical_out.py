6ea92d7712ed98679acd10991a24f0b736cf34fe
Azmaine Iqtidar <aiqtidar@princeton.edu>
2021-12-12 17:19:11 -0500

    Forgot to comment some stuff

--- tests/test_graphical_out.py
+++ tests/test_graphical_out.py
@@ -6,13 +6,13 @@
 #---------------------------------------------------------------------------------------------------
 import bulktest_generator as gen
 
-import os
-import sys
-import inspect
+# import os
+# import sys
+# import inspect
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
+# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.insert(0, parentdir) 
 
 import graphical_out
 import system_factory

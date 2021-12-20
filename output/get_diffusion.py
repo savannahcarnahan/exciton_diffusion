57f0f33edef04beb2bda839793fd64ae8c713003
savannahcarnahan <sdc2@princeton.edu>
2021-12-20 12:54:11 -0500

    fixed conflict

--- output/get_diffusion.py
+++ output/get_diffusion.py
@@ -2,25 +2,6 @@ import numpy as np
 import mysite as site
 import output.graphical_out as out
 
-<<<<<<< HEAD
-def get_diffusion(exc_list):
-    diff_dist = []
-    diff_dist.append(0)
-    start_site = exc_list
-    print(start_site)
-    pos1 = start_site[0][0].get_position()
-    i = 1
-    while i<len(exc_list):
-        for exc_site in exc_list[i]:
-            curr_site = exc_site
-            pos2 = curr_site.get_position()
-            dist = pos1 - pos2
-            dist = np.linalg.norm(dist)
-            diff_dist.append(dist)
-            i+=1
-    # print(diff_dist)
-    return diff_dist
-=======
 # def get_diffusion(exc_list):
 #     diff_dist = []
 #     diff_dist.append(0)
@@ -50,5 +31,4 @@ def get_diffusion(exc_list, start_site = None):
         dist = np.max(np.linalg.norm(sites_nice- start_pos))
         d_arr.append(dist)
     return d_arr
->>>>>>> 6a49ea357203f91e8018fc845d81344ec4c161d2
 
